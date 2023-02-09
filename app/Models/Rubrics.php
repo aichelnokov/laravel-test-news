dcc33fb40e2b182411d8c0566823e44bc45d6896
Aleksei Chelnokov <aichelnokov@gmail.com>
2023-02-09 11:51:26 +0300

    Add 'parent' and 'children' methods for Rubrics model.

--- app/Models/Rubrics.php
+++ app/Models/Rubrics.php
@@ -13,4 +13,14 @@ class Rubrics extends Model
     {
         return $this->belongsToMany(News::class, 'news_rubrics');
     }
+
+    public function parent()
+    {
+        return $this->belongsTo('App\Models\Rubrics', 'rubrics_id')->where('rubrics_id', 0)->with('parent');
+    }
+
+    public function children()
+    {
+        return $this->hasMany('App\Models\Rubrics', 'rubrics_id')->with('children');
+    }
 }
