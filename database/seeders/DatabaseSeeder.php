ed0f54f0786c4d2bfe603aa4ff94bad445f56e07
Aleksei Chelnokov <aichelnokov@gmail.com>
2023-02-09 21:15:54 +0300

    Add rubrics seeds.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\models\Rubrics;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('rubrics')->delete();
+        $common = Rubrics::firstOrCreate(['name' => 'Общество']);
+            $citylife = Rubrics::firstOrCreate(['name' => 'Городская жизнь'], ['rubrics_id' => $common->id]);
+            $elections = Rubrics::firstOrCreate(['name' => 'Выборы'], ['rubrics_id' => $common->id]);
+        $cityday = Rubrics::firstOrCreate(['name' => 'День города']);
+            $fireworks = Rubrics::firstOrCreate(['name' => 'Салюты'], ['rubrics_id' => $cityday->id]);
+            $playground = Rubrics::firstOrCreate(['name' => 'Детская площадка'], ['rubrics_id' => $cityday->id]);
+                $pgfirst = Rubrics::firstOrCreate(['name' => '0-3 года'], ['rubrics_id' => $playground->id]);
+                $pgsecond = Rubrics::firstOrCreate(['name' => '3-7 лет'], ['rubrics_id' => $playground->id]);
+        $sport = Rubrics::firstOrCreate(['name' => 'Спорт']);
+        $this->command->info('Rubrics table seeded!');
     }
 }
